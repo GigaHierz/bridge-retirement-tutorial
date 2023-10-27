4b56926f8e591cc93909ac445d1e50879b5b9b64
Lena Hierzi <lenahierzi@hotmail.com>
2023-10-27 09:04:21 +0100

    Add toucan quickstarter set up

--- packages/hardhat/contracts/interfaces/IInterchainGasPaymaster.sol
+++ packages/hardhat/contracts/interfaces/IInterchainGasPaymaster.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.13;
 
 interface IInterchainGasPaymaster {
     function payForGas(
-        uint32 messageId,
+        bytes32 messageId,
         uint32 _destination,
         uint32 gasAmount,
         address sender
-    ) external view returns (address);
+    ) external payable returns (address);
 }
